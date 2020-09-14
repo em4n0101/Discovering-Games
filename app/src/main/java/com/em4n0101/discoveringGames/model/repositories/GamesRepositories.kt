0547e08b649a818c5484066076e29285c9ef11de
Emmanuel Cruz <em4n0101@gmail.com>
2020-09-13 22:24:22 -0500

    (Database) Define

---
+++ app/src/main/java/com/em4n0101/discoveringGames/model/repositories/GamesRepositories.kt
@@ -0,0 +1,27 @@
+package com.em4n0101.discoveringGames.model.repositories
+
+import com.em4n0101.discoveringGames.model.Game
+import com.em4n0101.discoveringGames.model.Genre
+import com.em4n0101.discoveringGames.model.database.GameDatabaseDao
+import kotlinx.coroutines.flow.Flow
+
+class GamesRepositories(private val databaseDao: GameDatabaseDao) {
+    /**
+     * Database operations
+     */
+    suspend fun insertGame(game: Game) = databaseDao.insertGame(game)
+
+    fun getFavoriteGames(): Flow<List<Game>> = databaseDao.getAllGames()
+
+    fun getFavoriteGameBy(id: Int): Flow<Game?> = databaseDao.getGameBy(id)
+
+    suspend fun deleteFavoriteGameBy(id: Int) = databaseDao.deleteGameBy(id)
+
+    suspend fun insertGenre(genre: Genre) = databaseDao.insertGenre(genre)
+
+    fun getFavoriteGenres(): Flow<List<Genre>> = databaseDao.getAllGenres()
+
+    fun getFavoriteGenreBy(id: Int): Flow<Genre?> = databaseDao.getGenreBy(id)
+
+    suspend fun deleteFavoriteGenre(id: Int) = databaseDao.deleteGenreBy(id)
+}
